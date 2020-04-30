1dc28cd07e92b00e5376af17b47709d80f8aff76
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-04-30 16:37:04 +0700

    start redux

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,6 +12,10 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+// Redux stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
+
 const styles = (theme) => ({
   ...theme.formSubmit
 })
@@ -32,6 +36,7 @@ class login extends Component {
       email: this.state.email,
       password: this.state.password
     }
+    this.props.loginUser(userData, this.props.history);
   }
 
   handleChange = (event) => {
@@ -41,8 +46,8 @@ class login extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-    const { errors, loading } = this.state;
+    const { classes, UI: { loading } } = this.props;
+    const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm></Grid>
@@ -95,7 +100,19 @@ class login extends Component {
 }
 
 login.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(login);
+const mapStateToProps = (state) => ({
+  user: state.user,
+  UI: state.UI
+})
+
+const mapActionsToProps = {
+  loginUser
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(login));
