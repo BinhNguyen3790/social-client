40949dd10e607f09480779e7bf8db0605892e164
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-05-01 23:31:26 +0700

    fix redux

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import userReducer from './reducers/userReducers';
+import uiReducer from './reducers/uiReducers';
+import dataReducer from './reducers/dataReducers';
+
+const initialState = {};
+const middleware = [thunk];
+
+const reducers = combineReducers({
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer
+});
+const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
+export default store;
