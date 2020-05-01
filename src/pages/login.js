f6af4a7ad954a7ef165543b44ff2597097756644
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-05-01 09:59:43 +0700

    fix auth state

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,33 +12,9 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = {
-  form: {
-    textAlign: 'center'
-  },
-  image: {
-    width: '100px',
-    margin: '20px auto 20px auto'
-  },
-  pageTitle: {
-    margin: '10px auto 10px auto'
-  },
-  textField: {
-    margin: '10px auto 10px auto'
-  },
-  button: {
-    marginTop: 20,
-    position: 'relative'
-  },
-  customeError: {
-    color: 'red',
-    fontSize: '0.8rem',
-    marginTop: 10
-  },
-  progress: {
-    position: 'absolute'
-  }
-}
+const styles = (theme) => ({
+  ...theme.formStyles
+})
 
 class login extends Component {
   constructor() {
@@ -63,6 +39,7 @@ class login extends Component {
     axios.post('/login', userData)
       .then(res => {
         console.log(res.data);
+        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
         this.setState({
           loading: false
         });
