5b96dcb3ddc0495cb6ac6043827ce6cb8303c84f
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-05-05 21:54:07 +0700

    Edit profile

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -1,20 +1,79 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import widthStyles from '@material-ui/core/styles/withStyles';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+// Redux stuff
 import { connect } from 'react-redux';
+import { editUserDetails } from '../redux/actions/userActions';
+
+// MUI stuff
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import { Tooltip } from '@material-ui/core';
+
+// Icons
+import EditIcon from '@material-ui/iconsEdit';
 
 const styles = (theme) => ({
   ...theme
 })
 
 class EditDetails extends Component {
-  render() {
-    return (
-      <div>
+  state = {
+    bio: '',
+    website: '',
+    location: '',
+    open: false
+  };
 
-      </div>
+  handleOpen = () => {
+    this.setState({ open: true })
+    mapUserDetailsToState(this.props.credentials);
+  }
+  handleClose = () => {
+    this.setState({ open: false });
+  }
+  componentDidMount() {
+    const { credentials } = this.props;
+    mapUserDetailsToState(credentials);
+  }
+  mapUserDetailsToState = (credentials) => {
+    this.setState({
+      bio: credentials.bio ? credentials.bio : '',
+      website: credentials.website ? credentials.website : '',
+      location: credentials.location ? credentials.location : '',
+    })
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <Tooltip title='Edit details' placement='top'>
+          <IconButton onClick={this.handleOpen} className={classes.button}>
+            <EditIcon color='primary' />
+          </IconButton>
+        </Tooltip>
+        <Dialog open={this.state.open} onClose={this.handleClose} fullWidth maxWidth="sm">
+
+        </Dialog>
+      </Fragment>
     )
   }
 }
 
-export default EditDetails
+EditDetails.propTypes = {
+  editUserDetails: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  credentials: state.user.credentials
+})
+
+export default connect(mapStateToProps, { editUserDetails })(withStyles(styles)(EditDetails));
