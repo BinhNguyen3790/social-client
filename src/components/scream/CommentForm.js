41fb31a1b0f2463ea66d1b457af0a7d874cf65d6
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-05-29 21:50:17 +0700

    fix

--- src/components/scream/CommentForm.js
+++ src/components/scream/CommentForm.js
@@ -1,64 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
-
-// MUI stuff
+// MUI Stuff
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-
 // Redux stuff
 import { connect } from 'react-redux';
 import { submitComment } from '../../redux/actions/dataActions';
 
-const styles = theme => ({
-  ...theme.formStyles,
-})
+const styles = (theme) => ({
+  ...theme.formStyles
+});
 
 class CommentForm extends Component {
   state = {
     body: '',
     errors: {}
-  }
+  };
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.UI.errors) {
       this.setState({ errors: nextProps.UI.errors });
     }
     if (!nextProps.UI.errors && !nextProps.UI.loading) {
-      this.setState({ body: '' })
+      this.setState({ body: '' });
     }
   }
+
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
-  }
+  };
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.submitComment(this.props.screamId, { body: this.state.body })
-  }
+    this.props.submitComment(this.props.screamId, { body: this.state.body });
+  };
+
   render() {
     const { classes, authenticated } = this.props;
     const errors = this.state.errors;
+
     const commentFormMarkup = authenticated ? (
       <Grid item sm={12} style={{ textAlign: 'center' }}>
         <form onSubmit={this.handleSubmit}>
           <TextField
-            name='body'
-            type='text'
-            label='Comment on scream'
+            name="body"
+            type="text"
+            label="Comment on scream"
             error={errors.comment ? true : false}
             helperText={errors.comment}
             value={this.state.body}
             onChange={this.handleChange}
             fullWidth
-            className={classes.textField} />
-          <Button type='submit'
-            variant='contained'
-            color='primary'
-            className={classes.button}>Submit</Button>
+            className={classes.textField}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Submit
+          </Button>
         </form>
         <hr className={classes.visibleSeparator} />
       </Grid>
-    ) : null
+    ) : null;
     return commentFormMarkup;
   }
 }
@@ -69,11 +76,14 @@ CommentForm.propTypes = {
   classes: PropTypes.object.isRequired,
   screamId: PropTypes.string.isRequired,
   authenticated: PropTypes.bool.isRequired
-}
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   UI: state.UI,
   authenticated: state.user.authenticated
-})
+});
 
-export default connect(mapStateToProps, { submitComment })(withStyles(styles)(CommentForm));
+export default connect(
+  mapStateToProps,
+  { submitComment }
+)(withStyles(styles)(CommentForm));
--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -99,6 +99,23 @@ export const deleteScream = (screamId) => (dispatch) => {
       dispatch({ type: DELETE_SCREAM, payload: screamId })
     })
     .catch(err => console.log(err));
+};
+
+export const getUserData = (userHandle) => dispatch => {
+  dispatch({ type: LOADING_DATA });
+  axios.get(`/user/${userHandle}`)
+    .then(res => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: res.data.screams
+      });
+    })
+    .catch(() => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: null
+      })
+    })
 }
 
 export const clearErrors = () => dispatch => {
