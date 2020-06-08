577d2fdc527dcddc071692ff0c1104578e0b3f22
BinhNguyen <nguyenvanbinh3790@gmail.com>
2020-06-08 08:56:42 +0700

    add base url

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ import axios from 'axios';
 
 const theme = createMuiTheme(themeFile);
 
+axios.defaults.baseURL = 'https://asia-east2-sociallappp.cloudfunctions.net/api';
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
